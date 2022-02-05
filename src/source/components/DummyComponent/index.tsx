1a0df5381e0115d09734e9d716ea1f8bdb4bf013
Anshul Laddha <laddhaanshul@gmail.com>
2022-02-06 00:59:47 +0530

    1. Folder setup 2. Plop template setup 3. Testing setup

---
+++ src/source/components/DummyComponent/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const DummyComponent: React.FC = () => {
+    return <div className="DummyComponent">
+        <h1>Helping Hands</h1>
+    </div>
+};
+
+export default DummyComponent;
---
+++ src/source/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { DummyComponent } from './components';
+
+function App() {
+  return (
+    <div className="App">
+      <DummyComponent/>
+    </div>
+  );
+}
+
+export default App;
