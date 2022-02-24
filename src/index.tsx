22233138a1597767524c7c697cc1f72ce1ba1c01
Anshul Laddha <laddhaanshul@gmail.com>
2022-02-25 00:00:27 +0530

    Package lock changes (#8)
    * Package lock changes
    
    * Lint fixes

--- src/index.tsx
+++ src/index.tsx
@@ -10,8 +10,6 @@ import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
-  useQuery,
-  gql,
 } from '@apollo/client';
 const client = new ApolloClient({
   uri: 'https://48p1r2roz4.sse.codesandbox.io', // Change it once we get the appolo server uri
