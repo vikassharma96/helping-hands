facd6b308a503c3078866a31c16134574207fedd
Anshul Laddha <laddhaanshul@gmail.com>
2022-03-06 00:04:24 +0530

    Added styling packages and  menu bar layout (#18)

---
+++ src/screens/homeScreen/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TopMenuBar from '../../components/topMenuBar';
+
+const HomeScreen: React.FC = (props: any) => {
+  return (
+    <div>
+      <TopMenuBar />
+    </div>
+  );
+};
+
+export default HomeScreen;
